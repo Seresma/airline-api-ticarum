ca38f1e109392f0ff4908e75c75604b0f02bd9f8
Sergio Escudero Manzano <sergio.escuderom@um.es>
2023-02-06 02:08:46 +0100

    Remove unused imports

--- src/main/java/com/airline/api/persistence/repositories/IFlightRepository.java
+++ src/main/java/com/airline/api/persistence/repositories/IFlightRepository.java
@@ -1,11 +1,7 @@
 package com.airline.api.persistence.repositories;
 
-import com.airline.api.persistence.domain.FlightStatusEnum;
 import com.airline.api.persistence.domain.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface IFlightRepository extends JpaRepository<Flight, Long> {
 }
--- src/main/java/com/airline/api/persistence/repositories/IFlightStatusRepository.java
+++ src/main/java/com/airline/api/persistence/repositories/IFlightStatusRepository.java
@@ -1,7 +1,6 @@
 package com.airline.api.persistence.repositories;
 
 import com.airline.api.persistence.domain.FlightStatus;
-import com.airline.api.persistence.domain.Plane;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IFlightStatusRepository extends JpaRepository<FlightStatus, Long> {
